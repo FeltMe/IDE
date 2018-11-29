24f4881fbc60df2897d8ed0121d95ff1cce0a876
Sasha <sdrozdiuk12@gmail.com>
2018-11-29 17:05:15 +0300

    + New wind

--- IDE/MainWindow.xaml.cs
+++ IDE/MainWindow.xaml.cs
@@ -21,7 +21,7 @@ namespace IDE
     /// </summary>
     public partial class MainWindow : Window
     {
-        protected XmlDocument doc = new XmlDocument();
+        //protected XmlDocument doc = new XmlDocument();
         protected string FilePath { get; set; }
         public MainWindow()
         {
@@ -30,44 +30,50 @@ namespace IDE
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                string FilePath = @"C:\Users\sddrozd\source\repos\IDE";
-                doc.InnerText = "Kappa ";
-                doc.Save(FilePath);
-                FilePath = $@"C:\Users\sddrozd\source\repos\IDE\{doc.Name}";
-            }
-            catch(Exception ex)
-            {
-               MessageBox.Show($"{ex.ToString()}");
-            }
+           //try
+           //{
+           //    string FilePath = @"C:\Users\sddrozd\source\repos\IDE";
+           //    doc.InnerText = "Kappa ";
+           //    doc.Save(FilePath);
+           //    FilePath = $@"C:\Users\sddrozd\source\repos\IDE\{doc.Name}";
+           //}
+           //catch(Exception ex)
+           //{
+           //   MessageBox.Show($"{ex.ToString()}");
+           //}
         }   
 
         private void MenuItem_Click_1(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                doc.LoadXml(System.IO.File.ReadAllText(FilePath));
-                XmlElement root = doc.DocumentElement;
-                XmlElement Param = doc.CreateElement("PrimaryCalendarPeriods");
-                Param.SetAttribute("ID", "ЗначениеID");
-                root.AppendChild(Param);
-                doc.Save(FilePath);
-                XmlNode Node = doc.GetElementById("ЗначениеID");
-                Param = doc.CreateElement("ResourceName");
-                // добавляем что надо
-                Node.AppendChild(Param);
-                doc.Save(FilePath);
-            }
-            catch(Exception ex)
-            {
-                MessageBox.Show($"{ex.ToString()}");
-            }
+            //try
+            //{
+            //    doc.LoadXml(System.IO.File.ReadAllText(FilePath));
+            //    XmlElement root = doc.DocumentElement;
+            //    XmlElement Param = doc.CreateElement("PrimaryCalendarPeriods");
+            //    Param.SetAttribute("ID", "ЗначениеID");
+            //    root.AppendChild(Param);
+            //    doc.Save(FilePath);
+            //    XmlNode Node = doc.GetElementById("ЗначениеID");
+            //    Param = doc.CreateElement("ResourceName");
+            //    // добавляем что надо
+            //    Node.AppendChild(Param);
+            //    doc.Save(FilePath);
+            //}
+            //catch(Exception ex)
+            //{
+            //    MessageBox.Show($"{ex.ToString()}");
+            //}
         }
 
         private void MenuItem_Click_2(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void MenuItem_Click_3(object sender, RoutedEventArgs e)
+        {
+           //Window NewProject = new Window();
+           //NewProject.ShowDialog();
         }
     }
 }
